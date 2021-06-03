e9ccaec174feeb6c9f609fc71b7a309cfff65090
Joy Kirii <joykirii@gmail.com>
2021-06-03 16:48:14 +0300

    create serializer for neighborhood model

---
+++ neighborhood_app/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Neighbourhood
+
+
+class NeighbourhoodClass(serializers.ModelSerializer):
+  class Meta:
+    model = Neighbourhood
+    fields = "__all__"
+
+  def create(self, validated_data):
+    neighbour = Neighbourhood.objects.create(
+    neighbourhood_name=validated_data['neighbourhood_name'],
+    location=validated_data['location'],
+    count=validated_data['count'],
+    admin=validated_data['admin'],
+    )
+    
+    neighbour.save()
+    return neighbour
