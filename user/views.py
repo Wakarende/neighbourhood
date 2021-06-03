2eecfd894f24986108bc1fb8768316e3e7d5ca53
Joy Kirii <joykirii@gmail.com>
2021-06-03 17:07:07 +0300

    create user model get and post function

--- user/views.py
+++ user/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .serializer import UserSerializerClass
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import UserModel
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+class UserRegistrationView(generics.GenericAPIView):
+  serializer_class = UserSerializerClass
+  all_users = User.objects.all()
+
+  def post(self, request, *args, **kwargs):
+    serializer = self.serializer_class(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+
+    user_data = serializer.data
+
+    response = {
+      'data': {
+        "user": dict(user_data),
+        "status": "success",
+        "message": "Account created successfully"
+      }
+    }
+
+    return Response(response, status=status.HTTP_201_CREATED)
+
+  def get(self, request, *args, **kwargs):
+    serializers = UserSerializerClass(self.all_users, many=True)
+    return Response(serializers.data)
